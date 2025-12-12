38efe7494f88281db80913dd71bbef234845de3a
Aybrea <wrenchou22@gmail.com>
2025-12-12 10:43:37 +0800

    outputFileTracingIncludes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/voice-convert': ['./proto/**/*'],
+    },
+  },
+};
 
 module.exports = nextConfig;
