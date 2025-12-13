82ee816053dc44d6855f544d049caa720f53a556
Aybrea <wrenchou22@gmail.com>
2025-12-13 11:37:17 +0800

    config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      '/api/voice-convert': ['./proto/**/*'],
-    },
+  outputFileTracingIncludes: {
+    '/api/voice-convert': ['./proto/**/*'],
   },
 };
 
